4f4da3a4c86a92f985e7c6efa3abd1941445e583
RyanHedges <ryanhedges@gmail.com>
2013-12-02 22:42:26 -0800

    add javascript for random link selection for 'awesome' add jquery for document ready continue styling for mobile devices

---
+++ assets/javascripts/actions.js
@@ -0,0 +1,38 @@
+var directToRandomLink = {
+
+  initialize: function() {
+    var awesomeLink = document.getElementById("awesome-link")
+    awesomeLink.addEventListener("click", function(){
+      var yourLink = randomize.getLink()
+      awesomeLink.href = yourLink
+    });
+  }
+}
+
+var randomize = {
+
+  myLinks: new Array(
+    "http://www.instapulse.herokuapp.com/",
+    "https://github.com/gopher-snakes-2013/Instapulse",
+    "http://www.starwarsracer.herokuapp.com/",
+    "https://github.com/gopher-snakes-2013/orphan_snakes",
+    "http://interview-overflow.herokuapp.com/",
+    "https://github.com/gopher-snakes-2013/interview-overflow"
+  ),
+
+  randomNumber: function(collectionOfLinks){
+  var randomNumber = Math.round(Math.random()*(collectionOfLinks.length-1))
+  return randomNumber
+  },
+
+  getLink: function(){
+  var myRandomLink = randomize.randomNumber(randomize.myLinks)
+  return this.myLinks[myRandomLink]
+  }
+
+}
+
+
+$(document).ready(function() {
+  directToRandomLink.initialize()
+})
