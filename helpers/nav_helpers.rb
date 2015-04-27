bdc7386ebcad99ec23f015354f06f8ad128f74a6
Ryan Hedges <ryanhedges@gmail.com>
2015-04-27 00:29:48 -0700

    Remove puts call in helper
    I did this because the middleman console was calling it with every page load.
    It was to help with debugging for another problem and is not needed.

--- helpers/nav_helpers.rb
+++ helpers/nav_helpers.rb
@@ -8,7 +8,6 @@ module NavHelpers
   def nav_link(link_text, url, options = {})
     options[:class] ||= ""
     options[:class] << "active" if url == current_page.url
-    puts(current_page)
     link_to(link_text, url, options)
   end
 end
