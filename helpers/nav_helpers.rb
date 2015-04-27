7adf74d3d8b61df5856b97eb0c16c44bc465653d
Ryan Hedges <ryanhedges@gmail.com>
2015-04-26 23:04:09 -0700

    Identify the current page the user is on
    I did this because I want the user to know what page they are currently on
    when viewing the website. It will underline the navigation with the current
    page. This was hard because it's a static site so the "active" class had to be
    added through a helper method instead of Javascript or something done on the
    server side. Javascript is used to select the parent of that active link so
    the styling of the `li` and the `a` tag could happen.

--- helpers/nav_helpers.rb
+++ helpers/nav_helpers.rb
@@ -4,4 +4,11 @@ module NavHelpers
       return "disabled"
     end
   end
+
+  def nav_link(link_text, url, options = {})
+    options[:class] ||= ""
+    options[:class] << "active" if url == current_page.url
+    puts(current_page)
+    link_to(link_text, url, options)
+  end
 end
